98ef470c9b4dbcd3ddada265b18c0d72318ee3fa
ducielo <99044962+ducielo@users.noreply.github.com>
2025-08-21 13:40:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // Note: base: './' = assets relatifs => OK sur GitHub Pages (sous-chemin) et Hostinger
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/mon-portfolio/',
   server: { port: 5173, open: true },
   build: { outDir: 'dist', sourcemap: false }
 })
